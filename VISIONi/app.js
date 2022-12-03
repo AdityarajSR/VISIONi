110a5c61661225d72ba68cf5931d744f5c2d7ac8
Adityaraj Singh Rajput <91075262+AdityarajSR@users.noreply.github.com>
2022-12-03 17:51:05 +0530

    4

--- VISIONi/app.js
+++ VISIONi/app.js
@@ -1,10 +1,16 @@
-const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
+const bodyparser = require('body-parser');
+
+const path = require('path');
 
 const app = express();
 
+
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended:false}));
 
 app.get('/', function(req, res){
     const filePath = path.join(__dirname, 'views', 'index.html');
@@ -19,6 +25,25 @@ app.get('/equipments', function(req, res){
 app.get('/tractors', function(req, res){
     const filePath = path.join(__dirname, 'views', 'tractors.html');
     res.sendFile(filePath);
+})
+
+app.get('/addtractor', function(req, res){
+    const filePath = path.join(__dirname, 'views', 'addtractor.html')
+    res.sendFile(filePath);
+})
+
+app.post('/addtractor', function(req, res){
+    const tractors =  req.body;
+    
+    const filePath = path.join(__dirname, 'data', 'tractors.json');
+    const tractordata = fs.readFileSync(filePath);
+    const storeddata = JSON.parse(tractordata);
+
+    storeddata.push(tractors);
+    
+    fs.writeFileSync(filePath, JSON.stringify(tractors));
+
+    res.redirect('/tractors');
 })
 
 app.listen(3000);
