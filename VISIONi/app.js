ee81727aba32150755107422a3dbddd4e4e9512a
Adityaraj Singh Rajput <91075262+AdityarajSR@users.noreply.github.com>
2022-12-04 13:05:59 +0530

    4 DEC

--- VISIONi/app.js
+++ VISIONi/app.js
@@ -1,49 +1,48 @@
 const fs = require('fs');
 
-const express = require('express');
-const bodyparser = require('body-parser');
-
 const path = require('path');
+
+const express = require('express');
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.use(express.urlencoded({extended:false}));
 
 app.get('/', function(req, res){
-    const filePath = path.join(__dirname, 'views', 'index.html');
-    res.sendFile(filePath);
+    res.render('index');
 });
 
 app.get('/equipments', function(req, res){
-    const filePath = path.join(__dirname, 'views' , 'equipments.html');
-    res.sendFile(filePath);
+    res.render('equipments');
 });
 
 app.get('/tractors', function(req, res){
-    const filePath = path.join(__dirname, 'views', 'tractors.html');
-    res.sendFile(filePath);
+    res.render('tractors');
 })
 
 app.get('/addtractor', function(req, res){
-    const filePath = path.join(__dirname, 'views', 'addtractor.html')
-    res.sendFile(filePath);
+    res.render('addtractor');
 })
 
 app.post('/addtractor', function(req, res){
-    const tractors =  req.body;
+    const tractors = req.body;
     
     const filePath = path.join(__dirname, 'data', 'tractors.json');
     const tractordata = fs.readFileSync(filePath);
     const storeddata = JSON.parse(tractordata);
 
     storeddata.push(tractors);
-    
-    fs.writeFileSync(filePath, JSON.stringify(tractors));
+
+    fs.writeFileSync(filePath, JSON.stringify(storeddata));
 
     res.redirect('/tractors');
+
 })
 
 app.listen(3000);
