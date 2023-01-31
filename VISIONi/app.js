5c0ff071629167b27ab7d5e66418fe60c0d0764c
Adityaraj Singh Rajput <91075262+AdityarajSR@users.noreply.github.com>
2023-02-01 01:46:51 +0530

    1FEB

--- VISIONi/app.js
+++ VISIONi/app.js
@@ -4,11 +4,26 @@ const express = require("express");
 
 const app = express();
 
+const db = require("./data/database");
+
 const uuid = require("uuid");
+
+const session = require("express-session");
+
+const mongodbStore = require('connect-mongodb-session');
+
+const MongoDBStore = mongodbStore(session);
+
+const sessionStore = new MongoDBStore({
+    uri :'mongodb+srv://CONNECtIt:CONNECtIt@cluster0.jzll7mx.mongodb.net/?retryWrites=true&w=majority',
+    databaseName : 'auth-demo',
+    collection : 'sessions'
+  })
 
 const defroute = require("./routes/default_routes");
 const tracroute = require("./routes/tracroute");
 const harroute = require("./routes/harroute");
+const demoRoutes = require("./routes/demo");
 
 app.set("views", path.join(__dirname, "views"));
 // console.log(path.join(__dirname, "views"));
@@ -19,16 +34,27 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', defroute);
-app.use('/', tracroute);
-app.use('/', harroute);
+app.use(session({
+  secret : 'VISIONi',
+  resave : false,
+  saveUninitialized : false,
+  store : sessionStore
+}));
 
-app.use(function(req, res){
-    res.status(404).render('404');
-});
+app.use(demoRoutes);
+app.use("/", defroute);
+app.use("/", tracroute);
+app.use("/", harroute);
 
-// app.use(function(error, req, res, next){
-//     res.status(500).render('500');
+
+// app.use(function (req, res) {
+//   res.status(404).render("404");
 // });
 
-app.listen(3000);
+// app.use(function (error, req, res, next) {
+//   res.status(500).render("500");
+// });
+
+db.connectToDatabase().then(function () {
+  app.listen(3000);
+});
---
+++ VISIONi/data/database.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function connectToDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://CONNECtIt:CONNECtIt@cluster0.jzll7mx.mongodb.net/?retryWrites=true&w=majority'
+  );
+  database = client.db('auth-demo');
+}
+
+function getDb() {
+  if (!database) {
+    throw { message: 'You must connect first!' };
+  }
+  return database;
+}
+
+module.exports = {
+  connectToDatabase: connectToDatabase,
+  getDb: getDb,
+};
